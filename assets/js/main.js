8dbb3793b9abd566fa101231e604206ef3fe95dc
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-01-31 17:54:36 +0700

    zoom board with padding

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,17 @@ const board = document.getElementById('board');
 
 Chessground(board);
 
-window.addEventListener('resize', function() {
+function resizeBoard() {
   const ev = document.createEvent('Event');
   ev.initEvent('chessground.resize', false, false);
   document.body.dispatchEvent(ev);
+}
+
+window.addEventListener('resize', resizeBoard);
+
+document.getElementById('zoom').addEventListener('input', function(e) {
+  const zoom = parseInt(e.target.value);
+  const border = Math.round((100 - zoom) / 4);
+  board.parentNode.setAttribute("style", `padding: ${border}%;`);
+  resizeBoard();
 });
