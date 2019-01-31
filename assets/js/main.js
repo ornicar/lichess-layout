6c4db284d622471cde942957c2d878b1e9fc6f3f
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-01-31 22:00:25 +0700

    icon font and detailed buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,7 @@ function resizeBoard() {
 
 window.addEventListener('resize', resizeBoard);
 
-document.getElementById('zoom').addEventListener('input', function(e) {
-  document.body.setAttribute('style', `--zoom: ${parseInt(e.target.value)}`);
-  resizeBoard();
-});
+// document.getElementById('zoom').addEventListener('input', function(e) {
+//   document.body.setAttribute('style', `--zoom: ${parseInt(e.target.value)}`);
+//   resizeBoard();
+// });
