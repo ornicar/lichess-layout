94cded1f68ee7365ce6c7c3973fef82fc47947cd
Thibault Duplessis <thibault.duplessis@gmail.com>
2019-02-07 05:59:37 +0700

    tournament layouts WIP

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,3 +1,16 @@
 document.querySelector('#topnav-toggle').addEventListener('change', e => {
   document.body.classList.toggle('masked', e.target.checked);
 });
+
+window.onload = function() {
+  Array.from(document.getElementsByClassName('mini-board'))
+    .forEach(Chessground);
+}
+
+function resizeBoard() {
+  const ev = document.createEvent('Event');
+  ev.initEvent('chessground.resize', false, false);
+  document.body.dispatchEvent(ev);
+}
+
+window.addEventListener('resize', resizeBoard);
